5fe49319ed615c36e2413998d8bea6a68a7fca39
ahmakbardev <ahmakbar.dev@gmail.com>
2024-04-09 18:23:44 +0700

    First Push omahnalar Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#5D50C6',
+        'own-pink': '#F85E9F'
+      }
+    },
+  },
+  plugins: [],
+}
+
